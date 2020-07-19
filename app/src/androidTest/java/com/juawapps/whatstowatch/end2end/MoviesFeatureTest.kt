9d4a5dfcc1ab253eb1b06d048ad30edbebeeb9b6
vec <joaocevada@gmail.com>
2020-07-19 14:45:33 +0100

    Cleans up ActivityScenario in MoviesFeatureTest

--- app/src/androidTest/java/com/juawapps/whatstowatch/end2end/MoviesFeatureTest.kt
+++ app/src/androidTest/java/com/juawapps/whatstowatch/end2end/MoviesFeatureTest.kt
@@ -9,6 +9,7 @@ import com.juawapps.whatstowatch.utils.mockserver.MockServerTestRule
 import dagger.hilt.android.testing.HiltAndroidRule
 import dagger.hilt.android.testing.HiltAndroidTest
 import dagger.hilt.android.testing.UninstallModules
+import org.junit.After
 import org.junit.Rule
 import org.junit.Test
 
@@ -24,6 +25,11 @@ class MoviesFeatureTest {
     var mockServerTestRule = MockServerTestRule()
 
     private lateinit var activityScenario: ActivityScenario<MainActivity>
+
+    @After
+    fun cleanup() {
+        activityScenario.close()
+    }
 
     @Test
     fun auctionsListViewDisplayed() {
